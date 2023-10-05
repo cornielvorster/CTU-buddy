5d1ecd4e2dd6ee2da54dc4253e7d0bc575eb134b
cornielvorster <cornielvorster@gmail.com>
2023-10-05 10:11:26 +0200

    delete everything

--- static/scripts/contact.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-});
